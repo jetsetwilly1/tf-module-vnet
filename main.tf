aed6ba0c0e9c61a25967432142eb3704ba920053
P10175382 <stuart.elcocks@capita.co.uk>
2020-07-16 16:33:52 +0100

    Initial checkin of vnet module.

---
+++ main.tf
@@ -0,0 +1,39 @@
+#Azure Generic vNet Module
+data azurerm_resource_group "vnet" {
+  name = var.resource_group_name
+}
+
+resource azurerm_virtual_network "vnet" {
+  name                = var.vnet_name
+  resource_group_name = data.azurerm_resource_group.vnet.name
+  location            = data.azurerm_resource_group.vnet.location
+  address_space       = var.address_space
+  dns_servers         = var.dns_servers
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  count                = length(var.subnet_names)
+  name                 = var.subnet_names[count.index]
+  resource_group_name  = data.azurerm_resource_group.vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [ var.subnet_prefixes[count.index] ]
+  service_endpoints     = var.subnet_endpoints[count.index]
+}
+
+data "azurerm_subnet" "import" {
+  for_each             = var.nsg_ids
+  name                 = each.key
+  resource_group_name  = data.azurerm_resource_group.vnet.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+
+  depends_on = [azurerm_subnet.subnet]
+}
+
+resource "azurerm_subnet_network_security_group_association" "vnet" {
+  for_each                  = var.nsg_ids
+  subnet_id                 = data.azurerm_subnet.import[each.key].id
+  network_security_group_id = each.value
+
+  depends_on = [data.azurerm_subnet.import]
+}
---
+++ variables.tf
@@ -0,0 +1,52 @@
+variable "vnet_name" {
+  description = "Name of the vnet to create"
+  default     = "acctvnet"
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group to be imported."
+}
+
+variable "address_space" {
+  type        = list(string)
+  description = "The address space that is used by the virtual network."
+  default     = ["10.0.0.0/16"]
+}
+
+# If no values specified, this defaults to Azure DNS 
+variable "dns_servers" {
+  description = "The DNS servers to be used with vNet."
+  default     = []
+}
+
+variable "subnet_prefixes" {
+  description = "The address prefix to use for the subnet."
+  default     = ["10.0.1.0/24"]
+}
+
+variable "subnet_names" {
+  description = "A list of public subnets inside the vNet."
+  default     = ["subnet1", "subnet2", "subnet3"]
+}
+
+variable "subnet_endpoints" {
+  description = "A list of service endpoints for the subnet names."
+  default     = [[]]
+}
+
+variable "nsg_ids" {
+  description = "A map of subnet name to Network Security Group IDs"
+  type        = map(string)
+
+  default = {
+  }
+}
+
+variable "tags" {
+  description = "The tags to associate with your network and subnets."
+  type        = map(string)
+
+  default = {
+    ENV = "test"
+  }
+}
